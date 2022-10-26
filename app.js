f05a6ae1422d8b41ee3334b0dfd46db84b090315
Rebecca <108008511+Rclarkeweb@users.noreply.github.com>
2022-10-26 16:53:15 +0100

    Added comments to app.js

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-// const colors = ["green", "red", "orange", "purple"];
-
+// Hex code array
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
+// Select button
 const btn = document.getElementById('btn');
 
+// Select span where generated hex code will go
 const color = document.querySelector(".color");
 
+// Create button that on click will run a for loop that iterates 6 times to generate a hex code
 btn.addEventListener("click", function() {
     let hexColor = '#';
     for (let i = 0; i < 6; i++) {
@@ -15,6 +17,7 @@ btn.addEventListener("click", function() {
     document.body.style.backgroundColor = hexColor;
 });
 
+// Generate a random number 
 const getRandomNumber = () => {
     return Math.floor(Math.random() * hex.length);
 };
